47aa98ee302c15823eaaad222324ae334bfd79a9
Apoorva Shukla <apoorvashukla744@gmail.com>
2024-03-29 01:29:50 +0530

    added code

--- src/Arrays/array1.java
+++ src/Arrays/array1.java
@@ -3,6 +3,28 @@ package Arrays;
 public class array1 {
     public static void main(String[] args) {
         int[] arr={10,20,30,40,50};
+        display(arr);
+        reverse(arr);
+        display(arr);
+    }
 
+    public static void display(int[] arr) {
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+
+    public static void reverse(int[] arr) {
+        int i=0;
+        int j=arr.length -1;
+        while(i<=j){
+            int temp =arr[i];
+            arr[i]=arr[j];
+            arr[j]=temp;
+
+            i++;
+            j--;
+        }
     }
 }
