908aec7ad220cdff0355b7985458537dc33263f3
Apoorva Shukla <118666954+apoorva0777@users.noreply.github.com>
2024-06-06 22:59:08 +0530

    added code

---
+++ src/String/stringbuilder.java
@@ -0,0 +1,13 @@
+package String;
+
+public class stringbuilder {
+    public static void main(String[] args) {
+        StringBuilder sb=new StringBuilder("Tony");
+        System.out.println(sb.charAt(1));
+        sb.setCharAt(1,'p');
+        System.out.println(sb);
+        sb.insert(0,'K');
+        System.out.println(sb);
+
+    }
+}
---
+++ src/swapNumbers.java
@@ -0,0 +1,13 @@
+public class swapNumbers {
+    public static void main(String[] args) {
+        int a=5;
+        int b=4;
+        int temp=0;
+        temp=a;
+        a=b;
+        b=temp;
+        System.out.println("reversed numbers : ");
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
