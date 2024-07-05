a5ff9e0a462fc39fb16ce409ef6f4fc946f5e13e
Apoorva Shukla <118666954+apoorva0777@users.noreply.github.com>
2024-07-05 23:37:58 +0530

    added code

--- src/Arrays/reverse.java
+++ src/Arrays/reverse.java
@@ -1,5 +1,39 @@
 package Arrays;
 
-public class reverse {
+import java.util.Scanner;
 
+public class reverse {
+    public static void reversearr(int[] arr, int n) {
+        int start = 0;
+        int end = n - 1;
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+
+            start++;
+            end--;
+        }
+        System.out.println("reversed arr:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i]+" ");
+        }
+
+    }
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Original arr:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+        reversearr(arr, n);
+    }
 }
