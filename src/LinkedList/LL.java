fbeba9089cbd5374f00e6f0579aa681881fe455c
Apoorva Shukla <118666954+apoorva0777@users.noreply.github.com>
2024-07-09 23:59:26 +0530

    added code

---
+++ src/LinkedList/LL.java
@@ -0,0 +1,15 @@
+package LinkedList;
+
+public class LL {
+    class Node{
+        String data;
+        Node next;
+    }
+
+
+    //insert
+
+    public static void main(String[] args) {
+         LL list =new LL();
+    }
+}
